44dd729801afbc09f5d42f8ce15cd28c4052cb1c
abvai <vais12122009@gmail.com>
2022-03-01 12:18:06 -0500

    commited sneab

--- src/main/java/com/qaactitime/pages/EnterTimePage.java
+++ src/main/java/com/qaactitime/pages/EnterTimePage.java
@@ -3,5 +3,35 @@ package com.qaactitime.pages;
 import com.qaactitime.base.DriverScript;
 
 public class EnterTimePage extends DriverScript {
+	// *********************************************** Page Elements ************************************************************* //
+	
+		@FindBy(id="logoutLink") WebElement linkLogout;
+		@FindBy(xpath="//a[@class='userProfileLink username ']") WebElement textUserLoggedIn;
+		@FindBy(id="container_tasks") WebElement menuTasks;
+		
+	// *********************************************** Page Initialization ***************************************************** //
+		
+		public EnterTimePage()
+		{
+			PageFactory.initElements(driver, this);
+		}
+		
+	// *********************************************** Page Methods ******* ***************************************************** //
+		
+		
+		public void clickTasksMenu()
+		{
+			menuTasks.click();
+		}
+		
+		public String verifyUserLoggedIn()
+		{
+			return textUserLoggedIn.getText();
+		}
+		
+		public void clickLogout()
+		{
+			linkLogout.click();
+		}
 
 }
--- src/main/java/com/qaactitime/pages/LoginPage.java
+++ src/main/java/com/qaactitime/pages/LoginPage.java
@@ -1,7 +1,54 @@
 package com.qaactitime.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.qaactitime.base.DriverScript;
 
 public class LoginPage extends DriverScript{
+	// *********************************************** Page Elements ************************************************* //
+	
+		@FindBy(id="username") WebElement tbUsername;
+		@FindBy(name="pwd") WebElement tbPassword;
+		@FindBy(xpath="//div[text()='Login ']") WebElement btnLogin;
+		@FindBy(xpath="//div[@class='atLogoImg']") WebElement actiLogo;
+		
+		// *********************************************** Page Initialization ******************************************** //
+		
+		// PageFactory is a class in Selenium which is used to initilize Current Page Class Elements
+		
+		public LoginPage()
+		{
+			PageFactory.initElements(driver, this);
+		}
+		
+		// *********************************************** Page Actions ****** ******************************************** //
+		
+		public boolean verifyActiLogo()
+		{
+			return actiLogo.isDisplayed();
+		}
+		
+		public String getLoginPageTitle()
+		{
+			return driver.getTitle();
+		}
+		
+		public void enterUsername(String username)
+		{
+			tbUsername.sendKeys(username);
+		}
+		
+		public void enterPassword(String password)
+		{
+			tbPassword.sendKeys(password);
+		}
+		
+		public void clickLoginButton()
+		{
+			btnLogin.click();
+		}
+		
 
 }
